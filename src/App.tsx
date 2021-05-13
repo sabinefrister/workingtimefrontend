dccfe6a0545051fd71068e5b8b0e617ecaebc994
sabinefrister <sabine@frister.net>
2021-05-13 20:57:45 +0200

    Set up new environment and start adding a form input.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import OverTimeForm from './OverTimeForm';
 
-function App() {
+
+function App(): JSX.Element{
+  useEffect(()=> {document.title = "My Working Time"}) 
+  const [overTimeValue, setOverTimeValue] = useState("")
+
+  const getOverTimeValue = (overTime: string) => {
+    setOverTimeValue(overTime);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Working Time</h1>
+      <OverTimeForm sendOverTimeValue={getOverTimeValue} />
+      <p>{overTimeValue}</p>
     </div>
   );
 }
---
+++ src/OverTimeForm.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+
+type Props = {
+	sendOverTimeValue: (overTime: string) => void
+}
+
+function OverTimeForm(props: Props): JSX.Element {
+  return (
+    <div>
+      <Formik 
+		initialValues={{
+			overTime: "",
+		}}
+		onSubmit={(overTimeValue) => {
+        	props.sendOverTimeValue(overTimeValue.overTime);
+        }}
+      >
+  		<Form>
+			<label htmlFor="overTime">Your Overtime from Yesterday</label>
+			<Field id="overTime" name="overTime" placeholder="00:00" />
+			<button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default OverTimeForm;
